c6a884bd3952adcb4f8fe724d4a4aa874551117e
Farhanunnasih <farhanunnasih@gmail.com>
2022-07-04 21:38:19 +0700

    Add impromptu swaggerr doc for phones resource

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const express = require('express')
 const { init } = require('./util')
 const bodyParser = require('body-parser')
+const routePhone = require('./src/routes/route-phone')
 const app = express()
+
+
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express');
+
 const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const routePhone = require('./src/routes/route-phone')
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Phones API - 2042408',
+      version: '1.0.0',
+      description: 'Use http://localhost:8080/api/{resoure endpoint} to access apis' 
+    },
+  },
+  apis: [
+    './src/routes/route-phone.js',
+    './src/controllers/controller-phone.js',
+  ], 
+}
+const swaggerSpec = swaggerJsDoc(options);
 
-app.use((err, req, res, next) => {
-    if (err.name === "UnauthorizedError") {
-        res.status(401).send("invalid token...");
-      } else {
-        next(err)
-      }
-})
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', routePhone)
+app.use('/', (req, res) => {
+  res.redirect('/docs')
+})
 app.use('*', (req, res) => {
   res.status(404).send("Invalid Resource Link");
 })
--- src/routes/route-phone.js
+++ src/routes/route-phone.js
@@ -2,14 +2,70 @@ const router = require('express').Router()
 const { phone } = require('../controllers')
 
 
+/**
+ * @openapi
+ * /api/phones:
+ *  get:
+ *      produces:
+ *          - application/json
+ *      parameters:
+ *          - name: name
+ *            in: formData
+ *            type: string
+ *      description: Retrieve all phones resources from api.
+ *      responses:
+ *          200:
+ *              description: return a data array containing phones object.
+ * 
+ */
 router.get('/phones', phone.getPhone)
 
+/**
+ * @openapi
+ * /api/phones/:id:
+ *  get:
+ *      description: Retrieve a phone resource with id from api.
+ *      responses:
+ *          200:
+ *              description: return a phone object with matching id.
+ * 
+ */
 router.get('/phones/:id', phone.getPhoneById)
 
+/**
+ * @openapi
+ * /api/phones/:
+ *  post:
+ *      description: Add a new phone resource to api.
+ *      responses:
+ *          201:
+ *              description: return a newly-created phone object with generated id.
+ * 
+ */
 router.post('/phones', phone.postPhone)
 
+/**
+ * @openapi
+ * /api/phones/:id:
+ *  put:
+ *      description: Edit a phone resource properties with matching id.
+ *      responses:
+ *          200:
+ *              description: return an updated phone object.
+ * 
+ */
 router.put('/phones/:id', phone.putPhoneById)
 
+/**
+ * @openapi
+ * /api/phones/:id:
+ *  delete:
+ *      description: Delete a phone resource with matching id
+ *      responses:
+ *          200:
+ *              description: return a deleted phone object, which no longer exist in the api.
+ * 
+ */
 router.delete('/phones/:id', phone.deletePhoneById)
 
 module.exports = router
